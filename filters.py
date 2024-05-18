4c56f7daa0cadbaa997dbfdc6e82a3dbc564fadb
valenshinga <valentinshingaki@gmail.com>
2024-05-18 12:10:10 -0300

    filter de contraste listo

--- filters.py
+++ filters.py
@@ -10,9 +10,16 @@ def plano(img, color):
 
     return img
 
-
+def contraste(img, intensidad):
+    intensidad = int(intensidad)
+    factor = (259 * (intensidad + 255)) / (255 * (259 - intensidad))
+    for i in range(img.width):
+        for j in range(img.height):
+            colorContrastado = tuple(int(factor * (c-128) + 128) for c in img[i,j])
+            img[i,j] = colorContrastado
+    return img
 
 ### Aca tienen que crea una funcion por cada filtro a programar
 ### Luego los agregan al diccionario de filtros junto con su nombre
-filtros = {"plano": plano, }
+filtros = {"plano": plano, "contraste": contraste}
 filtros_paralelos = {}
